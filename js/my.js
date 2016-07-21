dcf3cb70ba1f2922a0b2aad489081388da2868d8
seanhugh <squash110@gmail.com>
2016-07-21 10:11:28 -0400

    Added Javascript to Add certain amount of images

--- js/my.js
+++ js/my.js
@@ -1,6 +1,27 @@
-$(document).ready(function() {
 
+var image1 = "beerguys.png";
+var title1 = "Beer Guys";
+var pg1 = "This is the Beer Guys Paragraph Yaw"
+
+var image2 = "winford-01.png";
+var title2 = "Beer Guys";
+var pg2 = "This is the Beer Guys Paragraph Yaw"
+
+function add5images() {
+
+  for (var i = 1; i <= 5; i++) {
+      console.log("i began");
+      i = i.toString();
+      var temp = "image" + (i);
+      $( "#thecontainer" ).append( "<div class='bigim'><img src='images/blog/"+ eval(temp) + "''></div>" );
+      console.log(temp);
+  };
+};
+
+$(document).ready(function() {
+      add5images();
   $(window).scroll(function () {
+
       //if you hard code, then use console
       //.log to determine when you want the 
       //nav bar to stick.  
